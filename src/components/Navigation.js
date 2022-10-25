415ecf181a3e295f65107083ecd34e817f2c0ae0
danivent23122020 <dani.vent@orange.fr>
2022-10-25 20:18:44 +0200

    commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,11 +5,17 @@ const Navigation = () => {
     return (
         <div className="navigation">
             <ul>
-                <NavLink to="/">
+                <NavLink
+                    to="/"
+                    className={(nav) => (nav.isActive ? "nav-active" : "")}
+                >
                     <li>Accueil</li>
                 </NavLink>
-                <NavLink to="/about">
-                    <li>A propos</li>
+                <NavLink
+                    to="/about"
+                    className={(nav) => (nav.isActive ? "nav-active" : "")}
+                >
+                    <li>à propos</li>
                 </NavLink>
             </ul>
         </div>
