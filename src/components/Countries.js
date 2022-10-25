684f7bed866e5d3330c3a93a0acc6e5ddfcb738d
danivent23122020 <dani.vent@orange.fr>
2022-10-25 21:55:25 +0200

    commit

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Countries = () => {
+    const [data, setData] = useState ([]);
+    // video à 1 h 12 mn
+    useEffect(() => {
+        axios
+            .get("https://restcountries.com/v3.1/all")
+            .then((res) => setData(res.data));
+    }, []);
+
     return (
-        <div className='countries'>
+        <div className="countries">
             <h1>Countries</h1>
         </div>
     );
