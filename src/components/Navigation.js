ebbedbd1b7bc48264dfef984ec37eeff8d73010e
danivent23122020 <dani.vent@orange.fr>
2022-10-27 20:13:44 +0200

    deploy

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
         <div className="navigation">
             <ul>
                 <NavLink
-                    to="/"
+                    to="/home"
                     className={(nav) => (nav.isActive ? "nav-active" : "")}
                 >
                     <li>Accueil</li>
